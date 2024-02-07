276e141976bf58718b25dfb645e270f35a1106f5
kamitoth <135623291+kamitoth@users.noreply.github.com>
2024-02-07 10:25:37 +0100

    Update call_cloudy_batch_process_function_v1_2.R

--- call_cloudy_batch_process_function_v1_2.R
+++ call_cloudy_batch_process_function_v1_2.R
@@ -1,2 +1,2 @@
 # Calls cloudy_batch_process_function_v1_2.R
-source("https://raw.githubusercontent.com/kamitoth/ddPCR-threshold-setting-/main/cloudy_batch_process_function_v1_2.R?token=GHSAT0AAAAAACN3VBD7DYEW7J7QG7Y3GHCEZODJM4A", encoding="latin1")
+source("https://raw.githubusercontent.com/kamitoth/ddPCR-threshold-setting-/main/cloudy_batch_process_function_v1_2.R", encoding="latin1")
