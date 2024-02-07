b1e62298dc8566f35cf5d161c6877e0513d927d1
kamitoth <135623291+kamitoth@users.noreply.github.com>
2024-02-07 10:18:19 +0100

    Update call_cloudy_batch_process_function_v1_2.R

--- call_cloudy_batch_process_function_v1_2.R
+++ call_cloudy_batch_process_function_v1_2.R
@@ -1,2 +1,2 @@
 # Calls cloudy_batch_process_function_v1_2.R
-setwd("U:/Laboratories/NAA Lab/Work instructions"); source("cloudy_batch_process_function_v1_2.R", encoding="latin1")
+source("https://raw.githubusercontent.com/kamitoth/ddPCR-threshold-setting-/main/cloudy_batch_process_function_v1_2.R?token=GHSAT0AAAAAACN3VBD6IXJK2MRLXHOHSRYYZODJLRQ", encoding="latin1")
